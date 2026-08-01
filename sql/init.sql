774192521a36c796b333790e068b1756d5fe0fda
caoshuai.cs <caoshuai.cs@bytedance.com>
2026-08-01 21:35:28 +0800

    feat: sql

--- sql/init.sql
+++
@@ -1,6 +0,0 @@
--- @author: caoshuai.cs
--- @date: 2026-07-12
--- @description: PostgreSQL 初始化脚本，仅创建扩展；表结构由 Alembic 迁移管理
-CREATE EXTENSION IF NOT EXISTS vector;
-CREATE EXTENSION IF NOT EXISTS hstore;
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
